b35dec73ae9769da9106c626e429d477737ef0bc
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-11-11 15:45:58 +0100

    add some development options

--- cmake/GetVersionFromGit.cmake
+++ cmake/GetVersionFromGit.cmake
@@ -6,4 +6,8 @@ string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" VERSION_MAJOR "${VERSION}")
 string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION}")
 string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${VERSION}")
 string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${VERSION}")
+
+if(FORCE_NEW_VERSION)
+    MATH(EXPR VERSION_PATCH "${VERSION_PATCH}+1")
+endif()
 set(VERSION_SHORT "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
