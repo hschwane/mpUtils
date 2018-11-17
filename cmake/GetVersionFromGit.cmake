1ef8b6e030ff483435e35242e9dc86729e631c38
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-11-17 22:25:39 +0100

    move cmake option to different file

--- cmake/GetVersionFromGit.cmake
+++ cmake/GetVersionFromGit.cmake
@@ -7,6 +7,7 @@ string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION}")
 string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${VERSION}")
 string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${VERSION}")
 
+option(FORCE_NEW_VERSION "Increase the version number from git by one patach. Enable during development to not mix up versions." OFF)
 if(FORCE_NEW_VERSION)
     MATH(EXPR VERSION_PATCH "${VERSION_PATCH}+1")
 endif()
