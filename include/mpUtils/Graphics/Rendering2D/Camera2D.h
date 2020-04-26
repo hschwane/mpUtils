73db85a36e2a51d36a08bbf13f1ff5cabe8881a9
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2020-04-26 17:44:57 +0200

    add files for 2d camera

---
+++ include/mpUtils/Graphics/Rendering2D/Camera2D.h
@@ -0,0 +1,44 @@
+/*
+ * mpUtils
+ * Camera2D.h
+ *
+ * @author: Hendrik Schwanekamp
+ * @mail:   hendrik.schwanekamp@gmx.net
+ *
+ * Implements the Camera2D class
+ *
+ * Copyright (c) 2020 Hendrik Schwanekamp
+ *
+ */
+
+#ifndef MPUTILS_CAMERA2D_H
+#define MPUTILS_CAMERA2D_H
+
+// includes
+//--------------------
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <cmath>
+#include <string>
+//--------------------
+
+// namespace
+//--------------------
+namespace mpu {
+namespace gph {
+//--------------------
+
+//-------------------------------------------------------------------
+/**
+ * class Camera2D
+ *
+ * usage:
+ *
+ */
+class Camera2D
+{
+
+};
+
+}}
+#endif //MPUTILS_CAMERA2D_H
---
+++ src/Graphics/Rendering2D/Camera2D.cpp
@@ -0,0 +1,26 @@
+/*
+ * mpUtils
+ * Camera2D.cpp
+ *
+ * @author: Hendrik Schwanekamp
+ * @mail:   hendrik.schwanekamp@gmx.net
+ *
+ * Implements the Camera2D class
+ *
+ * Copyright (c) 2020 Hendrik Schwanekamp
+ *
+ */
+
+// includes
+//--------------------
+#include "mpUtils/Graphics/Rendering2D/Camera2D.h"
+//--------------------
+
+// namespace
+//--------------------
+
+//--------------------
+
+// function definitions of the Camera2D class
+//-------------------------------------------------------------------
+
