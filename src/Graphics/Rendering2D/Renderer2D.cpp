29ed9e407dc148887095e6a56c4ddd79dd0ead56
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2020-04-27 04:02:32 +0200

    add alpha blending to 2d renderer

--- src/Graphics/Rendering2D/Renderer2D.cpp
+++ src/Graphics/Rendering2D/Renderer2D.cpp
@@ -43,6 +43,8 @@ Renderer2D::Renderer2D(const std::string& shaderPath)
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     vao.bind();
 }
